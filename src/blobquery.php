efcfa454b6f07280dde3a332ed72896b2f060fac
David Bittner <bittneradave@gmail.com>
2017-06-16 14:46:54 -0400

    It works! Finally!

--- src/blobquery.php
+++ src/blobquery.php
@@ -1,17 +1,41 @@
 <?php
 
+function recursiveRemoveDirectory($directory)
+{
+    foreach(glob("{$directory}/*") as $file)
+    {
+        if(is_dir($file)) { 
+            recursiveRemoveDirectory($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($directory);
+}
+
 $seshID = $_GET["sessionid"];
 $keys = $_GET["keylist"];
 
-echo shell_exec("java -jar blobwebhelper.jar -keys $keys -sessionid $seshID");
+shell_exec("java -jar blobwebhelper.jar -keys $keys -sessionid $seshID");
 
 $zipName = $seshID . ".zip";
-echo shell_exec("zip -r $zipName $seshID/*");
+shell_exec("zip -r $zipName $seshID/*");
 
-header("Content-type: application/zip"); 
-header("Content-Disposition: attachment; filename=exports.zip");
-header("Content-length: " . filesize($zipName));
-header("Pragma: no-cache"); 
-header("Expires: 0"); 
-readfile("$zipName");
+header('Content-Type: application/zip');
+header("Content-Disposition: attachment; filename = $zipName");
+header('Content-Length: ' . filesize($zipName));
+header('Content-Transfer-Encoding: binary');
+
+$handle = fopen($zipName, 'rb'); 
+$buffer = ''; 
+while (!feof($handle)) { 
+    $buffer = fread($handle, 4096); 
+    echo $buffer; 
+    ob_flush(); 
+    flush(); 
+} 
+fclose($handle); 
+
+unlink($zipName);
+recursiveRemoveDirectory($seshID);
 ?>
